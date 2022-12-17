f4bacc385e76136df5779d8c4db00d6dd001d421
digitalpoint-SQA-Summer <54211953+digitalpoint-SQA-Summer@users.noreply.github.com>
2022-12-17 12:15:04 -0500

    Uploading Demo1 class file

---
+++ bmsDeployment/src/main/java/bmsDeployment/Demo1.java
@@ -0,0 +1,11 @@
+package bmsDeployment;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello, This is Devops Bootcamp Class");
+	}
+
+}
